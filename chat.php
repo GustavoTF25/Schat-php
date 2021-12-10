4f19941f466a041b3c0a466e66ba84e4a439dad1
Gustavo Trajano <gustavotrajano3@gmail.com>
2021-12-10 13:56:55 -0300

    fixed loop error

---
+++ chat.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Bate Papo</title>
+<META HTTP-EQUIV="Refresh" CONTENT="5;URL=chat.php">
+<script language="javascript">
+function ultima() {
+location.href = "#ultima";
+}
+</script>
+</head>
+<body bgcolor="#ffffff" onLoad="javascript:ultima()">
+<font face="Verdana" size="2">
+<?php include("chat.txt"); ?>
+<a name="ultima"> </a>
+</font>
+</body>
+</html>
